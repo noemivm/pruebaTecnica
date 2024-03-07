49bc92241838291ce90a5fba0e9b13c484751d97
NoeVM <noemivm@outlook.com>
2024-03-07 22:48:16 +0000

    cambios visulaes

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,28 +1,28 @@
 <x-app-layout>
     <x-slot name="header">
-       <!-- <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <!-- <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Tasks') }}
         </h2>-->
         <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('tasks.index')" :active="request()->routeIs('tasks.index')">
-                        {{ __('Crear nueva tarea') }}
-                    </x-nav-link>
+            <x-nav-link :href="route('tasks.index')" :active="request()->routeIs('tasks.index')">
+                {{ __('Crear nueva tarea') }}
+            </x-nav-link>
         </div>
     </x-slot>
 
     @if(session('success'))
-        <div class="alert alert-success" style="background-color: #D4EDDA; color: #155724; border-color: #C3E6CB; padding: .75rem 1.25rem; margin-bottom: 1rem; border: 1px solid transparent; border-radius: .25rem;">
-            {{ session('success') }}
-        </div>
+    <div class="alert alert-success"
+        style="background-color: #D4EDDA; color: #155724; border-color: #C3E6CB; padding: .75rem 1.25rem; margin-bottom: 1rem; border: 1px solid transparent; border-radius: .25rem;">
+        {{ session('success') }}
+    </div>
     @endif
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-12 flex justify-center">
+        <div class="max-w-md mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                   
-
-                <h1><strong>Crear Nueva Tarea</strong></h1>
+                    
+                    <h1><strong>Crear Nueva Tarea</strong></h1>
 
                     <form action="{{ route('tasks.store') }}" method="POST">
                         @csrf
@@ -35,7 +35,8 @@
                         @enderror
 
                         <label for="description">Descripción:</label><br>
-                        <textarea id="description" name="description"></textarea><br> <!-- {{ old('description') }} para mantener los datos introducidos con anterioridad -->
+                        <textarea id="description" name="description"></textarea><br>
+                        <!-- {{ old('description') }} para mantener los datos introducidos con anterioridad -->
                         <label for="status">Estado:</label><br>
                         <select id="status" name="status" required>
                             <option value="Pendiente">Pendiente</option>
@@ -45,25 +46,26 @@
 
                         <!-- VALIDACION-->
                         @error('status')
-                            <p>{{ $message }}</p>
+                        <p>{{ $message }}</p>
                         @enderror
 
 
                         <label for="assigned_to">Asignar a:</label><br>
                         <select id="assigned_to" name="assigned_to" required>
                             @foreach ($users as $user)
-                                <option value="{{ $user->id }}">{{ $user->name }}</option> <!-- usuario selecciona a que usuario asignar la tarea -->
+                            <option value="{{ $user->id }}">{{ $user->name }}</option>
+                            <!-- usuario selecciona a que usuario asignar la tarea -->
                             @endforeach
                         </select>
 
                         <!-- VALIDACION-->
                         @error('assigned_to')
-                            <p>{{ $message }}</p>
+                        <p>{{ $message }}</p>
                         @enderror
                         <br><br>
-            
+
                         <x-primary-button type="submit">Crear Tarea</x-primary-button>
-                    
+
                     </form>
                 </div>
             </div>
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -10,7 +10,7 @@
     </div>
     @endif
 
-    <div class="overflow-x-auto" style="padding: 30px">
+    <div class="overflow-x-auto flex justify-center p-6">
         <table class="w-auto bg-white shadow-md rounded-lg overflow-hidden">
             <thead class="bg-gray-800 text-white">
                 <tr>
@@ -50,53 +50,4 @@
             </tbody>
         </table>
     </div>
-
-    <!--   <div class="overflow-x-auto" style="padding: 30px">
-        <table class="w-auto bg-white shadow-md rounded-lg overflow-hidden">
-            <thead class="bg-gray-800 text-white">
-                <th class="px-6 py-3 text-center">Tabla tareas pendientes</th>
-                <td>
-                </td>
-            </thead>
-            <tbody class="text-gray-700">
-                @foreach($tasks as $task)
-                @if($task->status == 'Pendiente')
-                <tr>
-                    <th class="px-6 py-3 text-left">Título</th>
-                    <td class="px-6 py-4">{{ $task->title }}</td>
-                </tr>
-                <tr>
-                    <th class="px-6 py-3 text-left">Descripción</th>
-                    <td class="px-6 py-4">{{ $task->description }}</td>
-                </tr>
-                <tr>
-                    <th class="px-6 py-3 text-left">Estado</th>
-                    <td class="px-6 py-4">{{ $task->status }}</td>
-                </tr>
-                <tr>
-                    <th class="px-6 py-3 text-left">Asignado a</th>
-                    <td class="px-6 py-4">{{ $task->user->name }}</td>
-                </tr>
-                <tr>
-                    <th class="px-6 py-3 text-left">Acciones</th>
-                    <td class="px-6 py-4">
-                        <a href="{{ route('tasks.edit', $task->id) }}"
-                            class="text-blue-500 hover:text-blue-700 mr-2">Editar</a>
-                        <form action="{{ route('tasks.destroy', $task->id) }}" method="POST"
-                            style="display: inline-block;">
-                            @csrf
-                            @method('DELETE')
-                            <x-primary-button type="submit" class="text-red-500 hover:text-red-700">Eliminar
-                            </x-primary-button>
-                        </form>
-                    </td>
-                </tr>
-                @endif
-                @endforeach
-
-            </tbody>
-        </table>
-    </div>
--->
-
 </x-app-layout>
